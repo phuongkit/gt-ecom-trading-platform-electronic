1086bfb27abfc76558555f2e3d54ad819ea46819
phuongkit <phuongdorc@gmail.com>
2022-11-18 23:56:16 +0700

    add filter, utils

--- gt-backend/src/main/java/gt/electronic/ecommerce/utils/JwtTokenUtil.java
+++ gt-backend/src/main/java/gt/electronic/ecommerce/utils/JwtTokenUtil.java
@@ -1,12 +1,17 @@
 package gt.electronic.ecommerce.utils;
 
+import gt.electronic.ecommerce.dto.response.ResponseObject;
 import gt.electronic.ecommerce.entities.User;
 import gt.electronic.ecommerce.exceptions.AccessTokenNotValidException;
+import gt.electronic.ecommerce.exceptions.ResourceNotValidException;
+import gt.electronic.ecommerce.services.UserService;
 import io.jsonwebtoken.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.Authentication;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
@@ -21,20 +26,22 @@ public class JwtTokenUtil {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(JwtTokenUtil.class);
 
-  @Value("${app.auth.tokenExpirationMsec}")
+  @Value("${app.jwt.jwtExpirationInMs}")
   private long EXPIRE_DURATION;
 
-//  @Value("${app.auth.tokenRefreshExpirationDateMsec}")
-//  private long REFRESH_EXPIRATION;
+  @Value("${app.jwt.refreshExpirationDateInMs}")
+  private long REFRESH_EXPIRATION;
 
-  @Value("${app.auth.tokenSecret}")
+  @Value("${app.jwt.secret}")
   private String SECRET_KEY;
 
-  public String generateAccessToken(Authentication authentication) {
-    User user = (User) authentication.getPrincipal();
+  @Autowired
+  private UserService userService;
+
+  public String generateAccessToken(User userPrincipal) {
     return Jwts.builder()
-        .setSubject(String.format("%s,%s", user.getId(),
-            user.getPhone() != null ? user.getPhone() : user.getEmail()))
+        .setSubject(String.format("%s,%s", userPrincipal.getId(),
+            userPrincipal.getPhone() != null ? userPrincipal.getPhone() : userPrincipal.getEmail()))
         .setIssuedAt(new Date())
         .setExpiration(new Date(System.currentTimeMillis() + EXPIRE_DURATION))
         .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
@@ -83,7 +90,7 @@ public class JwtTokenUtil {
       if (header == null) return null;
       token = header.split(" ")[1].trim();
     } catch (ArrayIndexOutOfBoundsException ex) {
-      LOGGER.error("Bearer is null" + ex.getMessage());
+      LOGGER.error("Bearer is null", ex.getMessage());
     }
     return token;
   }
@@ -99,19 +106,19 @@ public class JwtTokenUtil {
     return this.getSubject(getAccessToken(request)).split(",")[1];
   }
 
-//  public String setExpiredJwtToken(String token) {
-//    return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody().setExpiration(new Date())
-//        .getSubject();
-//  }
+  public String setExpiredJwtToken(String token) {
+    return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody().setExpiration(new Date())
+        .getSubject();
+  }
 
-//  public String refreshToken(String token) {
-//    final Date createdDate = new Date();
-//    final Date expirationDate = new Date(createdDate.getTime() + REFRESH_EXPIRATION);
-//
-//    final Claims claims = parseClaims(token);
-//    claims.setIssuedAt(createdDate);
-//    claims.setExpiration(new Date());
-//
-//    return Jwts.builder().setClaims(claims).signWith(SignatureAlgorithm.HS512, SECRET_KEY).compact();
-//  }
+  public String refreshToken(String token) {
+    final Date createdDate = new Date();
+    final Date expirationDate = new Date(createdDate.getTime() + REFRESH_EXPIRATION);
+
+    final Claims claims = parseClaims(token);
+    claims.setIssuedAt(createdDate);
+    claims.setExpiration(new Date());
+
+    return Jwts.builder().setClaims(claims).signWith(SignatureAlgorithm.HS512, SECRET_KEY).compact();
+  }
 }
