b201ba1dde84aa2f93d5dc6a6b940554fce9ea7d
phuongkit <phuongdorc@gmail.com>
2022-11-25 08:39:26 +0700

    add address, user, role rest full api

---
+++ gt-backend/src/main/java/gt/electronic/ecommerce/services/UserService.java
@@ -0,0 +1,34 @@
+package gt.electronic.ecommerce.services;
+
+import gt.electronic.ecommerce.dto.request.AuthRequest;
+import gt.electronic.ecommerce.dto.request.UserCreationDTO;
+import gt.electronic.ecommerce.dto.response.ResponseObject;
+import gt.electronic.ecommerce.dto.response.UserResponseDTO;
+import gt.electronic.ecommerce.entities.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @author minh phuong
+ * @created 07/09/2022 - 11:07 PM
+ */
+public interface UserService extends UserDetailsService {
+  Page<UserResponseDTO> getAllUsers(String keyword, Pageable pageable);
+
+  UserResponseDTO getUserById(Integer id);
+  
+  UserResponseDTO getUserByAccessToken(String accessToken);
+
+  User getUserByLoginKey(String loginKey);
+
+  UserResponseDTO registerUser(AuthRequest auth, boolean isSeller);
+
+  UserResponseDTO createUser(UserCreationDTO creationDTO, MultipartFile imageFile);
+
+  UserResponseDTO updateUser(Integer id, UserCreationDTO creationDTO, MultipartFile imageFile);
+
+  UserResponseDTO deleteUserById(Integer id);
+}
