38a62738b7554fc10a2ae0df347894156fdcb48d
Nguyen36 <88275484+Nguyen36@users.noreply.github.com>
2022-11-25 23:12:45 +0700

    //fix ui

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -4,3 +4,4 @@ export { userService } from './user.service';
 export { productService } from './product.service';
 export { categoryService } from './category.service';
 export { ratingService } from './rate.service';
+export { orderService } from './order.service';
