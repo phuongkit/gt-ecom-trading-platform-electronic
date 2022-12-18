b6ad7b1ef3be6a665c23a653d3cf7e0cef2a88b8
Nguyen36 <colalombo@gmail.com>
2022-12-18 17:28:26 +0700

    /

--- gt-frontend/admin/src/routes/route.jsx
+++ gt-frontend/admin/src/routes/route.jsx
@@ -1,19 +1,32 @@
 import Home from "../pages/home/Home";
-import Login from "../pages/login/Login";
-import NotFound from "../components/notfound/NotFound";
-import { Routes, Route } from "react-router-dom";
-import Layout from "../components/layout";
-import ListProduct from "../pages/list/ListProduct";
 import Order from "../pages/order/Order";
+import Login from "../pages/login/Login";
+import ListProduct from "../pages/list/ListProduct";
+import Product from "../pages/single/Product";
+import User from "../pages/single/User";
+import Comment from "../pages/comment/Comment";
+import New from "../pages/new/New";
+import NotFound from "../pages/notfound/NotFound";
+import { productInputs, userInputs } from "../formSource";
+import { Routes, Route } from "react-router-dom";
+import { ProtectedRoute } from "./protected.route";
+import ListUser from "../pages/list/ListUser";
+import ListReview from "../pages/list/ListReview";
+import CommentView from "./../pages/comment/CommentView";
+import ReviewsList from "../pages/review/ReviewsList";
+import ReviewsView from "../pages/review/ReviewsView";
+import Layout from "../components/layout";
+import EditProduct from "../pages/new/EditProduct";
+import NewProduct from "../pages/new/NewProduct";
 export const AdminRoutes = () => {
   return (
     <Routes>
       <Route
         path="/"
         element={
-     
+          <ProtectedRoute>
             <Layout />
-         
+          </ProtectedRoute>
         }
       >
         <Route index element={<Home />} />
@@ -21,16 +34,32 @@ export const AdminRoutes = () => {
           path="orders"
           element={<Order title="Đơn hàng - thegioididong" />}
         />
-        <Route path="*" element={<NotFound />} />
-      </Route>
-      <Route path="products">
+        <Route path="users">
+          <Route index element={<ListUser />} />
+          <Route path=":userId" element={<User />} />
+          <Route
+            path="new"
+            element={<New inputs={userInputs} title="Add New User" />}
+          />
+        </Route>
+        <Route path="products">
           <Route index element={<ListProduct />} />
           <Route
             path=":productId"
-           
+            element={<EditProduct title="Add New Product" />}
           />
-          <Route path="new" />
+          <Route path="new" element={<NewProduct title="Add New Product" />} />
         </Route>
+        <Route path="reviews">
+          <Route index element={<ReviewsList />} />
+          <Route path=":reviewId" element={<ReviewsView />} />
+        </Route>
+        <Route path="comments">
+          <Route index element={<Comment />} />
+          <Route path=":commentId" element={<CommentView />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Route>
       <Route path="/login" element={<Login />} />
     </Routes>
   );
