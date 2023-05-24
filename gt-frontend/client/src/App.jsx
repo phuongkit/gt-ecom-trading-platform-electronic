3eec60a1f9d358f6f891294f4222a3bfa9947b18
phuongkit <phuongdorc@gmail.com>
2023-05-24 22:00:40 +0700

    add google analytics

--- gt-frontend/client/src/App.jsx
+++ gt-frontend/client/src/App.jsx
@@ -1,9 +1,17 @@
+import { useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import MessengerCustomerChat from 'react-messenger-customer-chat';
 import Routes from './routes';
 import { HashRouter } from 'react-router-dom';
+import ReactGA from 'react-ga';
 import './App.css';
+
 function App() {
+    const TRACKING_ID = 'UA-263715340-1';
+    ReactGA.initialize(TRACKING_ID);
+    useEffect(() => {
+        ReactGA.pageview(window.location.pathname);
+    });
     if (window.location.hash === '#_=_') {
         window.location.hash = '';
     }
